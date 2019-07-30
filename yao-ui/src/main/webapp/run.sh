d47f0e29b4c51f426879547fba8e0e369ed0673d
y3tu <14643092@qq.com>
2019-07-30 18:48:02 +0800

    fix bug

--- yao-ui/src/main/webapp/run.sh
+++ yao-ui/src/main/webapp/run.sh
@@ -24,5 +24,11 @@ fi
 
 echo 'build image'
 docker build --rm -t docker_yao-ui:latest .
-echo 'run docker container'
-docker run -p 80:80 --name yao-ui -d docker_yao-ui:latest
+
+echo '开始推送镜像到docker hub ...'
+docker tag docker_yao-ui:latest y3tu/yao-ui:latest
+docker push y3tu/yao-ui:latest
+echo '镜像推送结束 end...'
+
+#echo 'run docker container'
+#docker run -p 80:80 --name yao-ui -d docker_yao-ui:latest
