a92355b9c0e7bcc0c6634a0a3a2e4ab1f83b1bec
y3tu <14643092@qq.com>
2019-08-17 16:14:10 +0800

    update

--- yao-ui/src/main/webapp/src/views/login/index.vue
+++ yao-ui/src/main/webapp/src/views/login/index.vue
@@ -1,48 +1,142 @@
 <template>
     <div class="login">
-        <el-form ref="loginForm" :model="loginForm" :rules="loginRules" label-position="left" label-width="0px"
-                 class="login-form">
-            <h3 class="title">Yao 快速开发平台 </h3>
-            <el-form-item prop="username">
-                <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
-                    <icon slot="prefix" name="user" style="height: 39px;width: 13px;margin-left: 2px;"></icon>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-                <el-input v-model="loginForm.password" type="password" auto-complete="off" placeholder="密码"
-                          @keyup.enter.native="handleLogin">
-                    <icon slot="prefix" name="lock" style="height: 39px;width: 13px;margin-left: 2px;"></icon>
-                </el-input>
-            </el-form-item>
-            <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-            <el-form-item style="width:100%;">
-                <el-button :loading="loading" size="medium" type="primary" style="width:100%;"
-                           @click.native.prevent="handleLogin">
-                    <span v-if="!loading">登 录</span>
-                    <span v-else>登 录 中...</span>
-                </el-button>
-            </el-form-item>
-            <p class="login-tip">系统默认用户名：admin 密码：admin</p>
-        </el-form>
+        <el-row type="flex" justify="center" @keydown.enter.native="submitLogin">
+            <el-col style="width: 368px;">
+                <el-row class="header">
+                    <img :src="logo" alt="logo" width="220px">
+                    <div class="description">快速开发脚手架</div>
+                </el-row>
+                <el-row>
+                    <el-tabs v-model="tabName">
+                        <el-tab-pane name="username">
+                            <span slot="label"><i class="el-icon-user"></i> 用户密码登录</span>
+                            <el-card shadow="never">
+                                <el-form v-if="tabName==='username'"
+                                         ref="loginForm"
+                                         label-position="top"
+                                         :rules="loginRules"
+                                         :model="loginForm"
+                                         size="default">
+                                    <el-form-item prop="username">
+                                        <el-input type="text" v-model="loginForm.username" placeholder="请输入用户名">
+                                            <i slot="prepend" class="el-icon-s-custom"></i>
+                                        </el-input>
+                                    </el-form-item>
+                                    <el-form-item prop="password">
+                                        <el-input type="password" v-model="loginForm.password" placeholder="请输入密码">
+                                            <i slot="prepend" class="el-icon-lock"></i>
+                                        </el-input>
+                                    </el-form-item>
+                                    <el-form-item prop="code">
+                                        <el-input type="text" v-model="loginForm.code" placeholder="请输入验证码">
+                                            <template slot="prepend">验证码</template>
+                                            <template slot="append">
+                                                <img class="login-code" :src="code.src" @click="refreshCode">
+                                            </template>
+                                        </el-input>
+                                    </el-form-item>
+                                </el-form>
+                            </el-card>
+                        </el-tab-pane>
+
+                        <el-tab-pane label="手机号登录" name="mobile">
+                            <span slot="label"><i class="el-icon-mobile-phone"></i> 手机号登录</span>
+                            <el-card shadow="never">
+                                <el-form v-if="tabName==='mobile'"
+                                         ref="mobileForm"
+                                         label-position="top"
+                                         :rules="mobileRules"
+                                         :model="mobileForm"
+                                         size="default">
+                                    <el-form-item prop="mobile">
+                                        <el-input type="text" v-model="mobileForm.mobile" placeholder="请输入手机号">
+                                            <i slot="prepend" class="fa fa-mobile" style="font-size:22px"></i>
+                                        </el-input>
+                                    </el-form-item>
+                                    <el-form-item prop="code">
+                                        <el-input type="text" v-model="mobileForm.code" placeholder="请输入6位验证码">
+                                            <template slot="append">
+                                                <el-button @click="sendMsg" :disabled="sms.isDisabled">{{sms.btnName}}
+                                                </el-button>
+                                            </template>
+                                        </el-input>
+                                    </el-form-item>
+                                </el-form>
+                            </el-card>
+                        </el-tab-pane>
+                    </el-tabs>
+                </el-row>
+                <el-row>
+                    <el-button :loading="loading"
+                               size="medium"
+                               type="primary"
+                               style="width:100%;"
+                               @click="submitLogin">
+                        <span v-if="!loading">登 录</span>
+                        <span v-else>登 录 中...</span>
+                    </el-button>
+                </el-row>
+                <el-row type="flex"
+                        justify="center"
+                        style="position: fixed;bottom: 4vh;width: 368px;color: rgba(0, 0, 0, 0.45);font-size: 14px;">
+                    Copyright © 2019 - Present<a href="http://www.y3tu.com" target="_blank"
+                                                 style="margin:0 5px;">Y3tu</a>
+                </el-row>
+            </el-col>
+        </el-row>
+
+
     </div>
 </template>
 
 <script>
     import Config from '@/config'
     import Cookies from 'js-cookie'
+    import logo from '@/assets/logo/logo.png'
+    import './style.scss'
 
     export default {
         name: 'Login',
         data() {
             return {
+                logo: logo,
+                tabName: 'username',
                 loginForm: {
                     username: 'admin',
                     password: 'admin',
+                    code: '',
                     rememberMe: false
                 },
                 loginRules: {
                     username: [{required: true, trigger: 'blur', message: '用户名不能为空'}],
-                    password: [{required: true, trigger: 'blur', message: '密码不能为空'}]
+                    password: [{required: true, trigger: 'blur', message: '密码不能为空'}],
+                    //code: [{required: true, trigger: 'blur', message: '验证码不能为空'}]
+                },
+                code: {
+                    len: 4,
+                    src: ''
+                },
+                mobileForm: {
+                    mobile: '',
+                    code: ''
+                },
+                mobileRules: {
+                    mobile: [
+                        {required: true, message: '请输入手机号', trigger: 'blur'},
+                        {
+                            pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,
+                            message: '手机号不合法',
+                            trigger: 'blur'
+                        }
+                    ],
+                    code: [
+                        {required: true, message: '请输入验证码', trigger: 'blur'}
+                    ]
+                },
+                sms: {
+                    btnName: '发送短信',
+                    isDisabled: false,
+                    time: 60
                 },
                 loading: false,
                 redirect: undefined
@@ -71,70 +165,51 @@
                     rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
                 }
             },
-            handleLogin() {
-                this.$refs.loginForm.validate(valid => {
-                    const user = this.loginForm
-                    if (valid) {
-                        this.loading = true
-                        if (user.rememberMe) {
-                            Cookies.set('username', user.username, {expires: Config.passCookieExpires})
-                            Cookies.set('password', user.password, {expires: Config.passCookieExpires})
-                            Cookies.set('rememberMe', user.rememberMe, {expires: Config.passCookieExpires})
+            /**
+             * 刷新验证码
+             */
+            refreshCode() {
+
+            },
+            submitLogin() {
+                if (this.tabName === "username") {
+                    //用户名密码登录
+                    this.$refs.loginForm.validate(valid => {
+                        const user = this.loginForm;
+                        if (valid) {
+                            this.loading = true;
+                            if (user.rememberMe) {
+                                Cookies.set('username', user.username, {expires: Config.passCookieExpires});
+                                Cookies.set('password', user.password, {expires: Config.passCookieExpires});
+                                Cookies.set('rememberMe', user.rememberMe, {expires: Config.passCookieExpires})
+                            } else {
+                                Cookies.remove('username');
+                                Cookies.remove('password');
+                                Cookies.remove('rememberMe')
+                            }
+                            this.$store.dispatch('Login', user).then(() => {
+                                this.loading = false;
+                                this.$router.push({path: this.redirect || '/'})
+                            }).catch(() => {
+                                this.loading = false
+                            })
                         } else {
-                            Cookies.remove('username')
-                            Cookies.remove('password')
-                            Cookies.remove('rememberMe')
+                            console.log('error submit!!')
+                            return false
                         }
-                        this.$store.dispatch('Login', user).then(() => {
-                            this.loading = false
-                            this.$router.push({path: this.redirect || '/'})
-                        }).catch(() => {
-                            this.loading = false
-                        })
-                    } else {
-                        console.log('error submit!!')
-                        return false
-                    }
-                })
-            }
+                    })
+                } else if (this.tabName === "mobile") {
+                    //手机号登录
+                    this.$refs.mobileForm.validate(valid => {
+                        //todo
+                    })
+                }
+            },
+            //发送短信
+            sendMsg() {
+
+            },
         }
     }
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
-    .login {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        background-image: url('../../assets/bg.png');
-        background-size: cover;
-    }
-
-    .title {
-        margin: 0px auto 30px auto;
-        text-align: center;
-        color: #707070;
-    }
-
-    .login-form {
-        border-radius: 6px;
-        background: #ffffff;
-        width: 365px;
-        padding: 25px 25px 5px 25px;
-
-        .el-input {
-            height: 38px;
-
-            input {
-                height: 38px;
-            }
-        }
-    }
-
-    .login-tip {
-        font-size: 13px;
-        text-align: center;
-        color: #bfbfbf;
-    }
-</style>
